38e8b266828b6ecb5e5b2c46f0598f7857be9bb2
Maximilien Baas-Thomas <maxsbt@gmail.com>
2016-05-29 17:37:17 -0400

    main.py uses function in cubelib.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # This is the important information that
 # needs to be at the top!
 
+import cubelib
 if __name__ == '__main__':
     print 'hello dude!'
+    x = float(raw_input('%s' % "Enter number to cube: "))
+    print cubelib.cube(x)
